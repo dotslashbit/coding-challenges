957be7ecdf1fe1049acb317e20dee83a66712fce
Sahil Mahapatra <sahilm231199@gmail.com>
2023-10-07 20:34:38 +0530

    added  command support

--- write_your_own_wc_tool/main.py
+++ write_your_own_wc_tool/main.py
@@ -5,10 +5,20 @@ def get_size_in_bytes(file):
     size = os.path.getsize(file)
     return size
 
+def get_number_of_lines(file):
+    f = open(file, 'r')
+    number_of_lines = len(f.readlines())
+    return number_of_lines
+
 
 if lst[0] == 'cwcc':
     if lst[1] == '-c':
         file = lst[2]
         size = get_size_in_bytes(file)
         print(size)
+    elif lst[1] == '-l':
+        file = lst[2]
+        number_of_lines = get_number_of_lines(file)
+        print(number_of_lines)
+
 
