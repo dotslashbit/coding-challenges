a96ab30e4ba97dd468597ba6e2900bfbf24e9a4e
Sahil Mahapatra <sahilm231199@gmail.com>
2023-10-07 20:47:52 +0530

    added  command support

--- write_your_own_wc_tool/main.py
+++ write_your_own_wc_tool/main.py
@@ -10,6 +10,15 @@ def get_number_of_lines(file):
     number_of_lines = len(f.readlines())
     return number_of_lines
 
+def get_number_of_words(file):
+    f = open(file, 'r')
+    number_of_words = 0
+    for line in f:
+        words = line.split()
+        number_of_words += len(words)
+    return number_of_words
+
+
 
 if lst[0] == 'cwcc':
     if lst[1] == '-c':
@@ -20,5 +29,8 @@ if lst[0] == 'cwcc':
         file = lst[2]
         number_of_lines = get_number_of_lines(file)
         print(number_of_lines)
-
-
+    elif lst[1] == '-w':
+        file = lst[2]
+        number_of_words = get_number_of_words(file)
+        print(number_of_words)
+    
