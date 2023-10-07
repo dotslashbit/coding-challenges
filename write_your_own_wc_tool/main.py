fdb59b5b366755ae0bc6b6cd0d0dec6f8bcdaee8
Sahil Mahapatra <sahilm231199@gmail.com>
2023-10-07 20:10:27 +0530

    added the  command support

--- write_your_own_wc_tool/main.py
+++ write_your_own_wc_tool/main.py
@@ -1,11 +1,14 @@
 import os
 lst = input().split()
+
+def get_size_in_bytes(file):
+    size = os.path.getsize(file)
+    return size
+
+
 if lst[0] == 'cwcc':
     if lst[1] == '-c':
         file = lst[2]
-        print(file)
-        f = open(file, 'r')
-        #content = f.read()
-        #size = len(content)
-        #print(size)
+        size = get_size_in_bytes(file)
+        print(size)
 
