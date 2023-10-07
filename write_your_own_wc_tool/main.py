1baf1e3f798bac9452f8f79ac8ff9568cff887a4
Sahil Mahapatra <sahilm231199@gmail.com>
2023-10-07 22:45:18 +0530

    added  command support and now closes the file whenever it is opened

--- write_your_own_wc_tool/main.py
+++ write_your_own_wc_tool/main.py
@@ -8,6 +8,7 @@ def get_size_in_bytes(file):
 def get_number_of_lines(file):
     f = open(file, 'r')
     number_of_lines = len(f.readlines())
+    f.close()
     return number_of_lines
 
 def get_number_of_words(file):
@@ -16,11 +17,18 @@ def get_number_of_words(file):
     for line in f:
         words = line.split()
         number_of_words += len(words)
+    f.close()
     return number_of_words
 
+def get_number_of_characters(file):
+    f = open(file, 'r')
+    data = f.read()
+    f.close()
+    number_of_characters = len(data)
+    return number_of_characters
 
 
-if lst[0] == 'cwcc':
+if lst[0] == 'ccwc':
     if lst[1] == '-c':
         file = lst[2]
         size = get_size_in_bytes(file)
@@ -33,4 +41,8 @@ if lst[0] == 'cwcc':
         file = lst[2]
         number_of_words = get_number_of_words(file)
         print(number_of_words)
+    elif lst[1] == '-m':
+        file = lst[2]
+        number_of_characters = get_number_of_characters(file)
+        print(number_of_characters)
     
