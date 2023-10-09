9a9b27f6c55e10dd53fb72c06c417d66bc2da69c
Sahil Mahapatra <sahilm231199@gmail.com>
2023-10-09 11:27:46 +0530

    added support for multiple datatype value support

--- write_your_own_json_parser/main.py
+++ write_your_own_json_parser/main.py
@@ -13,6 +13,9 @@ lines = f.read().strip()
 
 # dictionary to store all the key value pairs
 key_value_pairs = {}
+
+boolean_types = {'true', 'false'}
+null_type = {'null'}
 #print(lines)
 #print(lines[0], lines[-2])
 
@@ -39,15 +42,33 @@ elif lines[0] == '{' and lines[-1] == '}':
         key, value = attribute.split(':')
         key = key.strip()
         value = value.strip()
-        # print(key)
+        # print(type(key))
 
         # if key is not string type, then its invalid
         if key[0] != '\"' or key[-1] != '\"':
             print('key is not string type')
             sys.exit()
         
+        # checking value
+        # checking if value is boolean
+        if value in boolean_types:
+            if value == "true":
+                key_value_pairs[key[1:-1]] = True
+            else:
+                key_value_pairs[key[1:-1]] = False
+        elif value in null_type:    
+            key_value_pairs[key[1:-1]] = None
+        elif value.isdigit():
+            key_value_pairs[key[1:-1]] = int(value)
+        elif value[0] == '\"' and value[-1] == '\"':
+            key_value_pairs[key[1:-1]] = str(value[1:-1])
+        else:
+            print('Invalid Value')
+            sys.exit()
+
+
         # if the key is valid, add it to the dictionary
-        key_value_pairs[key[1:-1]] = value[1:-1]
+
 
     # printing all the key value pairs
     for k,v in key_value_pairs.items():
